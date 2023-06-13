60dc08cc7875d0bde8f87fa5dd4c927031c454fc
Yomi Olalude <yomiolalude@gmail.com>
2023-06-13 20:37:04 +0100

    react

--- chatapp/server/admin.py
+++
@@ -1,7 +0,0 @@
-from django.contrib import admin
-from server.models import Category, Channel, Server
-
-
-admin.site.register(Category)
-admin.site.register(Channel)
-admin.site.register(Server)
--- chatapp/server/apps.py
+++
@@ -1,9 +0,0 @@
-from django.apps import AppConfig
-
-
-class ServerConfig(AppConfig):
-    default_auto_field = "django.db.models.BigAutoField"
-    name = "server"
-
-    def ready(self):
-        from server import signals
